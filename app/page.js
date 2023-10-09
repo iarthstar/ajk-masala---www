9b176f4adcebc6f43e3b7a937aa1664a7cbb74f2
Arth K. Gajjar <iarthstar@gmail.com>
2023-10-09 11:56:58 +0530

    Updated : page.js with HH
    * upgraded next and tailwindcss

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center">
+    <div className="w-screen h-[100dvh] flex flex-col items-center justify-between p-4">
+      <div />
+      <div className="text-center">
       <h1 className="text-3xl font-bold text-red-700">AJK Masala</h1>
-      <br />
-      <h2 className="text-sm font-medium">Coming Soon...</h2>
+      <h2 className="mt-4 text-neutral-500 text-sm font-medium">Coming Soon...</h2>
+      </div>
+      <p className="text-center text-sm leading-loose text-neutral-500 md:text-left">Designed &amp; Developed by <a href="https://www.honeyhexa.com" target="_blank" rel="noreferrer" class="font-medium underline underline-offset-4 hover:text-blue-500">Honey Hexa</a>.</p>
     </div>
   );
 }
